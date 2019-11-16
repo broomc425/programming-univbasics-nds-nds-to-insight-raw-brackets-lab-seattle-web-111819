ff173c224b642ca06b0c72851230710c6c404790
Charlie Broom <broomc425@gmail.com>
2019-11-16 21:38:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,7 @@ def directors_totals(nds)
   result = {}
   director_index = 0
   
-  while director_index < nds.size do
+  while director_index < nds[:movies].length do
     director = nds[director_index]
     result[director[:name]] = gross_for_director(director)
     director_index += 1
@@ -29,14 +29,4 @@ def directors_totals(nds)
   result
 end
 
-def gross_for_director(director_data)
-  total = 0
-  index = 0
   
-  while index < director_data[:movies].length do
-    total += director_data[:movies][index][:worldwide_gross]
-    index += 1
-  end
-  total
-end
-
